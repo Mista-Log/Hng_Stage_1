1d3eed0ed5b21061424e6b7b24df476e72a3e3ba
Mista Log <oloyedeoibrahimsmile@gmail.com>
2025-02-03 10:52:01 +0100

    new commit

--- mathapp/views.py
+++ mathapp/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import requests
+from django.views.generic import View
+from django.http import JsonResponse
 
 
 
@@ -42,14 +44,15 @@ def get_digit_sum(n):
 
 
 
-class MathAPIView(APIView):
+class MathAPIView(View):
+
     def get(self, request, n):
         try:
             number = int(n)
-            is_prime = get_is_prime(n)
-            is_perfect = get_is_perfect(n)
-            properties = get_number_properties(n)
-            fun_fact = get_fun_fact(n)
+            is_prime = get_is_prime(number)
+            is_perfect = get_is_perfect(number)
+            properties = get_number_properties(number)
+            fun_fact = get_fun_fact(number)
             digit_sum = get_digit_sum(number)
             data = {
                 "number": number,
@@ -58,12 +61,34 @@ class MathAPIView(APIView):
                 "properties": properties,
                 "digit_sum": digit_sum,
                 "fun_fact": fun_fact,
-
             }
-            return Response(data, status=status.HTTP_200_OK)
+            return JsonResponse(data, status=200)
         except ValueError:
-            return Response({"number": "alphabet", "error": True}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse({"number": "alphabet", "error": True}, status=400)
         except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return JsonResponse({"error": str(e)}, status=500)
+# class MathAPIView(APIView):
+#     def get(self, request, n):
+#         try:
+#             number = int(n)
+#             is_prime = get_is_prime(n)
+#             is_perfect = get_is_perfect(n)
+#             properties = get_number_properties(n)
+#             fun_fact = get_fun_fact(n)
+#             digit_sum = get_digit_sum(number)
+#             data = {
+#                 "number": number,
+#                 "is_prime": is_prime,
+#                 "is_perfect": is_perfect,
+#                 "properties": properties,
+#                 "digit_sum": digit_sum,
+#                 "fun_fact": fun_fact,
+
+#             }
+#             return Response(data, status=status.HTTP_200_OK)
+#         except ValueError:
+#             return Response({"number": "alphabet", "error": True}, status=status.HTTP_400_BAD_REQUEST)
+#         except Exception as e:
+#             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
