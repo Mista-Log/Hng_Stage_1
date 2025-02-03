c02f79fec2eb2c769aa27103306821b5aa50c77d
Mista Log <oloyedeoibrahimsmile@gmail.com>
2025-02-03 19:58:14 +0100

    speed request

--- mathapp/admin.py
+++ mathapp/admin.py
@@ -1,4 +1,2 @@
 from django.contrib import admin
-# from .models import MathClass
-# Register your models here.
 
--- mathapp/views.py
+++ mathapp/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 import requests
-from django.views.generic import View
 from django.http import JsonResponse
 
 
@@ -44,7 +40,7 @@ def get_digit_sum(n):
 
 
 
-class MathAPIView(View):
+class MathAPIView(APIView):
 
     def get(self, request):
 
@@ -66,32 +62,5 @@ class MathAPIView(View):
                 "fun_fact": fun_fact,
             }
             return JsonResponse(data, status=200)
-        # except ValueError:
-        #     return JsonResponse({"number": "alphabet", "error": True}, status=400)
         except Exception as e:
             return JsonResponse({"number": "alphabet", "error": True}, status=400)
-# class MathAPIView(APIView):
-#     def get(self, request, n):
-#         try:
-#             number = int(n)
-#             is_prime = get_is_prime(n)
-#             is_perfect = get_is_perfect(n)
-#             properties = get_number_properties(n)
-#             fun_fact = get_fun_fact(n)
-#             digit_sum = get_digit_sum(number)
-#             data = {
-#                 "number": number,
-#                 "is_prime": is_prime,
-#                 "is_perfect": is_perfect,
-#                 "properties": properties,
-#                 "digit_sum": digit_sum,
-#                 "fun_fact": fun_fact,
-
-#             }
-#             return Response(data, status=status.HTTP_200_OK)
-#         except ValueError:
-#             return Response({"number": "alphabet", "error": True}, status=status.HTTP_400_BAD_REQUEST)
-#         except Exception as e:
-#             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
